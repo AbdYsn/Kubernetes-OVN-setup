533017d4aadb2b2c868d505fbd36afba0da9c74f
abdysn <abdallahyas@mellanox.com>
2019-11-11 15:47:54 +0200

    added the scripts

---
+++ crashkernal_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+crashkernel=$1
+
+dep_install(){
+	if [[ -z  `rpm -qa | grep $1` ]]
+	then
+		yum install $1
+		echo "installed $1"
+	else
+		echo "$1 is already installed"
+	fi
+}
+
+set_crashkernel(){
+	echo "setting crashkernal to $crashkernel"
+	if [[ -n `cat /etc/default/grub | grep -o crashkernel=[0-9a-zA-Z]*" "` ]]
+	then
+        	sed -i -e s/crashkernel=[0-9a-zA-Z]*" "/"crashkernel=$crashkernel "/ /etc/default/grub
+	else
+        	sed -i -e s/"GRUB_CMDLINE_LINUX=\""/"GRUB_CMDLINE_LINUX=\"crashkernel=$crashkernel "/ /etc/default/grub
+	fi
+}
+
+dep_install kexec-tools
+set_crashkernel 
+grub2-mkconfig -o /boot/grub2/grub.cfg
+

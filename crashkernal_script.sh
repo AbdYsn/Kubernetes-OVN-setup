74033720df8ae2153e8add7744d2b58089f760c4
abdysn <abdallahyas@mellanox.com>
2019-11-12 11:10:16 +0200

    used a variable to store the regex

--- crashkernal_script.sh
+++ crashkernal_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [[ -z $1 ]]
 then
@@ -21,9 +22,10 @@ kexec_install(){
 
 set_crashkernel(){
 	echo "setting crashkernal to $crashkernel"
-	if [[ -n `cat /etc/default/grub | grep -o crashkernel=[0-9a-zA-Z]*" "` ]]
+        re_expression="crashkernel=[0-9a-zA-Z]*\" \""
+	if [[ -n `cat /etc/default/grub | grep -o $re_expression` ]]
 	then
-        	sed -i -e s/crashkernel=[0-9a-zA-Z]*" "/"crashkernel=$1 "/ /etc/default/grub
+        	sed -i -e s/$re_expression/"crashkernel=$1 "/ /etc/default/grub
 	else
         	sed -i -e s/"GRUB_CMDLINE_LINUX=\""/"GRUB_CMDLINE_LINUX=\"crashkernel=$1 "/ /etc/default/grub
 	fi
